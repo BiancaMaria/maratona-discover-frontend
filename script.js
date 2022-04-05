09b3eb3640edb2eb462de3baed1d94eaaa470479
@BiancaMaria <bianca.reis@gmz.solutions>
2022-04-04 22:19:16 -0300

    :memo: Aula 1 e aula 2 no discover rocketseat

---
+++ script.js
@@ -0,0 +1,62 @@
+const Modal = {
+  open(){      
+    document.querySelector('.modal-overlay').classList.add('active');
+  },
+
+  close(){
+    document.querySelector('.modal-overlay').classList.remove('active');
+  }
+}
+
+//dados da tabela
+const transaction = [
+  {
+    id:1,
+    description: 'Luz',
+    amount:-25000,
+    date:23/01/2021,
+  },
+
+  {
+    id:2,
+    description: 'Criação website',
+    amount:500000,
+    date:23/01/2021,
+  },
+
+  {
+    id:3,
+    description: 'Internet',
+    amount:-12000,
+    date:23/01/2021,
+  }
+];
+
+const Transaction = {
+  incomes(){
+    //somar as entradas
+  },
+
+  expenses(){
+    //somar as saidas
+  },
+
+  saldo(){
+    //totalEntrada - totalSaida
+  }
+}
+
+//Preciso pegar os dados do array transaction
+//e preencher na tabela construída em HTML
+const DOM = {
+  innerHTMLTransaction(){
+    const html = `
+    <tr>
+      <td class="description">Luz</td>
+      <td class="expense"> -R$500,00</td>
+      <td class="date">23/01/2021</td>
+      <td><img src="assets/minus.svg" alt="Remover transação"/></td>
+    </tr>
+    `;
+  }
+}
